42e6045057554dc0bdea5a512a3d1751ad55cc17
irinanedelcu <irinanedelcu10@yahoo.com>
2020-05-04 20:19:46 +0300

    - adujsted the form, it is almost done - added some more ajax sa-i placa lu' grasu - added download content in tarife.xhtml - adjusted/created necesarry classes

--- src/main/java/com/dai/project/view/UserView.java
+++ src/main/java/com/dai/project/view/UserView.java
@@ -1,9 +1,12 @@
 package com.dai.project.view;
 
+import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.faces.application.FacesMessage;
 import javax.faces.context.FacesContext;
 import javax.inject.Named;
+import java.util.HashMap;
+import java.util.Map;
 
 @Named
 @RequestScoped
@@ -39,6 +42,64 @@ public class UserView {
 
     public void save() {
         FacesContext.getCurrentInstance().addMessage(null,
-                new FacesMessage("Welcome " + name));
+                new FacesMessage("Am inregistrat cererea ta,  " + name));
     }
+
+    private String camera;
+    private Map<String, String> camere;
+
+    @PostConstruct
+    public void init() {
+        camere = new HashMap<String, String>();
+        camere.put("Camera dubla ***", "Camera dubla ***");
+        camere.put("Camera single ***", "Camera single ***");
+        camere.put("Camera dubla ****", "Camera dubla ****");
+        camere.put("Camera single ****", "Camera single ****");
+    }
+
+    public void setCamera(String camera) {
+        this.camera = camera;
+    }
+
+    public String getCamera() {
+        return camera;
+    }
+
+    public void setCamere(Map<String, String> camere) {
+        this.camere = camere;
+    }
+
+    public Map<String, String> getCamere() {
+        return camere;
+    }
+
+    private boolean micDejun;
+    private boolean patSuplimentar;
+
+    public boolean isMicDejun() {
+        return micDejun;
+    }
+
+    public void setMicDejun(boolean micDejun) {
+        this.micDejun = micDejun;
+    }
+
+    public boolean isPatSuplimentar() {
+        return patSuplimentar;
+    }
+
+    public void setPatSuplimentar(boolean patSuplimentar) {
+        this.patSuplimentar = patSuplimentar;
+    }
+
+    public void addMessage() {
+        String summary1 = micDejun ? "Checked" : "Unchecked";
+        if (summary1.equals("Checked"))
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Ati adaugat mic dejun."));
+        String summary2 = patSuplimentar ? "Checked" : "Unchecked";
+        if (summary2.equals("Checked"))
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Ati adaugat pat suplimentar."));
+    }
+
+
 }
